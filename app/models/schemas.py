a86ff4b9ed40008b0499ad12e6273e709a134115
Joaquin Uriarte <juriartebruno@gmail.com>
2025-06-03 13:10:12 -0700

    Now tolerates nulls

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -13,4 +13,11 @@ class CallDataBase(BaseModel):
     load_destination: Optional[str] = Field(None, max_length=100, nullable=True)
     carrier_sentiment: Optional[str] = Field(None, max_length=50, nullable=True)
 
-    ## TODO: Add validation for the data using the validator (We could ensure the classifiers respected the tags)
+    # Convert "null" to None given that we can only send string null
+    @validator('call_date', 'base_price', 'final_price', 'load_origin', 'call_outcome', 'call_duration', 'is_negotiated', 'load_destination', 'carrier_sentiment', pre=True)
+    def convert_null_to_none(cls, v):
+        if v == "null":
+            return None
+        return v
+    
+    ## TODO: Add additional validation for the data using the validator (We could ensure the classifiers respected the tags)
